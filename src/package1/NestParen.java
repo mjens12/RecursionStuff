c857416fc703524f6c0c99c6974090826bdbff58
mjens12 <mjens12@gmail.com>
2018-07-31 16:10:43 -0400

    more

---
+++ src/package1/NestParen.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class NestParen {
+	public boolean nestParen(String str) {
+		  if(str.length()==0)
+		  return true;
+		  if (str.charAt(0)=='(' && str.charAt(str.length()-1) == ')')
+		  return nestParen(str.substring(1,str.length()-1));
+		  else return false;
+		}
+}
